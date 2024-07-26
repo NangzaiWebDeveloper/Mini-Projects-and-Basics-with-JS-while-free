183653fb41a16bc73f7da92c2e36147e3c5eb4bf
NangzaiWebDeveloper <abujamnangzai00@gmail.com>
2024-07-27 00:12:52 +0600

    add questions

--- 14.js
+++ 14.js
@@ -1,7 +1,6 @@
 let str = "Abuzam Nangzai Singha"
 
-const reverseString=(str)=>{
+const reverseString = (str) => {
     return str.split("").reverse().join("");
 }
-
 console.log(reverseString(str));
