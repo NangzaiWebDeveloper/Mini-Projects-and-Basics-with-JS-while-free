dabfb775caed9e4bc212d76b151882b6c6c80920
NangzaiWebDeveloper <abujamnangzai00@gmail.com>
2024-08-01 01:35:01 +0600

    new in js

---
+++ 21.js
@@ -0,0 +1,7 @@
+function hhello() {
+    let fName = "Abuzam Nangzai Singha"
+    this.fName = fName;
+}
+const result = new hhello();
+console.log(result);//return objet
+console.log(result.fName); //return only name
