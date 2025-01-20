e3b3423b1fb0f012cdf1eb9e12768a760c92c050
NangzaiWebDeveloper <abujamnangzai00@gmail.com>
2025-01-20 23:31:31 +0600

    comment update eval method

--- 25.js
+++ 25.js
@@ -3,13 +3,13 @@
 
 // EX-01: 
 const num = "10 + 20";//value is string
-console.log(num);
+console.log(num);//output: 10+20
 const result = eval(num)//convert string to number
-console.log(result);
+console.log(result);//output: 30
 
 
 // EX-02:
 const a = 102;
 const b = 203;
 const sum = eval("a+b")//convert string to number
-console.log(sum) 
+console.log(sum) //output: 305
